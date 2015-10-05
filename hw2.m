64ae02f1a7fc7804a8575d55cfe9d0edcdbb91d8
DeanLym <yiminliu@stanford.edu>
2015-10-05 15:22:25 -0700

    Add files.

--- hw2.m
+++ hw2.m
@@ -9,6 +9,25 @@ N_3=2000;
 [H2,y2,s2]=shaw(N_2);
 [H3,y3,s3]=shaw(N_3);
 
+
+[~,spec1,~] = svd(H1);
+[~,spec2,~] = svd(H2);
+[~,spec3,~] = svd(H3);
+spec1 = diag(spec1);
+spec2 = diag(spec2);
+spec3 = diag(spec3);
+
+figure(21);
+subplot(1,3,1);
+plot((1:N_1)',log(spec1));
+
+subplot(1,3,2);
+plot((1:N_2)',log(spec2));
+
+subplot(1,3,3);
+plot((1:N_3)',log(spec3));
+
+
 spec1=eig(H1);
 spec2=eig(H2);
 spec3=eig(H3);
--- q6.m
+++ q6.m
@@ -1,14 +1,16 @@
 function [] = q6()
+close all;
 data = load('2015Assign1_1.txt');
 t = data(:,1);
 y = data(:,2);
 % plot(s_true);
 n=400;
-std_v = 0.5;
+std_v = 0.1;
 var_s = var(y);
+% var_s = 1e-6;
 ind = 20;
 l_s = pi/2;
-H = shaw(n);
+H = shaw(n); 
 X = ones(n,1);
 %% case 1 Nugget Effect
 R = std_v*eye(n);
@@ -32,37 +34,56 @@ s_pinv = pinv(H)*y;
 
 figure(61+ind);
 hold on;
-plot(t,s1,'r');
-plot(t,s2,'b');
-plot(t,s3,'k');
-plot(t,s4,'m');
-plot(t,y,'--');
-legend('Nugget','Linear','Exponential','Gaussian','y');
+plot(t,s1,'r','linewidth',2);
+plot(t,s2,'b','linewidth',2);
+plot(t,s3,'k','linewidth',2);
+plot(t,s4,'m-.','linewidth',2);
+plot(t,y,'--','linewidth',2);
+legend('$\hat{s}$(t) Nugget Covariance','$\hat{s}$(t) Linear Covariance',...
+    '$\hat{s}$(t) Exponential Covariance','$\hat{s}$(t) Gaussian Covariance','y(t)',...
+    'Location','NorthEastOutside');
+set(legend,'Interpreter','latex');
+set(legend,'Fontsize',12);
+set(legend,'Fontname','Times New Roman');
 figure(62+ind);
 hold on;
-plot(t,diag(V1),'r');
-plot(t,diag(V2),'b');
-plot(t,diag(V3),'k');
-plot(t,diag(V4),'m');
-legend('Nugget','Linear','Exponential','Gaussian');
+plot(t,diag(V1),'r','linewidth',2);
+plot(t,diag(V2),'b','linewidth',2);
+plot(t,diag(V3),'k','linewidth',2);
+plot(t,diag(V4),'m-.','linewidth',2);
+legend('Std Nugget Covariance','Std Linear Covariance',...
+    'Std Exponential Covariance','Std Gaussian Covariance',...
+    'Location','NorthEastOutside');
+set(legend,'Interpreter','latex');
+set(legend,'Fontsize',12);
+set(legend,'Fontname','Times New Roman');
 figure(63+ind);
 hold on;
-plot(t,s_pinv,'r-.');
-plot(t,y,'--');
-legend('Pseudo-inverse Solution','y');
+plot(t,s_pinv,'r-.','linewidth',2);
+plot(t,y,'--','linewidth',2);
+legend('$\hat{s}$(t) Pseudo-inverse','y(t)');
+set(legend,'Interpreter','latex');
+set(legend,'Fontsize',12);
+set(legend,'Fontname','Times New Roman');
 %
 figure(64+ind);
 hold on;
-plot(t,H*s1,'r');
-plot(t,H*s2,'b');
-plot(t,H*s3,'k');
-plot(t,H*s4,'m');
-plot(t,y,'--');
-legend('Nugget','Linear','Exponential','Gaussian','y');
+plot(t,H*s1,'r','linewidth',2);
+plot(t,H*s2,'b','linewidth',2);
+plot(t,H*s3,'k','linewidth',2);
+plot(t,H*s4,'m','linewidth',2);
+plot(t(1:10:end),y(1:10:end),'o','linewidth',2);
+legend('$\hat{y}$(t) Nugget Covariance','$\hat{y}$(t) Linear Covariance',...
+    '$\hat{y}$(t) Exponential Covariance','$\hat{y}$(t)Gaussian Covariance','y(t)',...
+        'Location','NorthEastOutside');
+set(legend,'Interpreter','latex');
+set(legend,'Fontsize',12);
+set(legend,'Fontname','Times New Roman');
 %% Question 7
 nr=10;
 s_c = zeros(n,nr);
 figure(65+ind);
+% title('Conditional realizations of $\hat{s}$(t)','Interpreter','latex','Fontsize',12,'Fontname','Times New Roman');
 hold on;
 Q = ExponentialCov(n,t,var_s,l_s);
 % Q = gaussianQ(n,t,var_s,l_s);
@@ -78,6 +99,7 @@ plot(t,s3,'r');
 ylim([-3,3]);
 end
 
+
 figure(66+ind);
 hold on;
 for i=1:nr
@@ -88,12 +110,6 @@ plot(t,H*s_c(:,i));
 plot(t,y,'r');
 % ylim([-3,3]);
 end
-
-
-
-
-
-
 
 
 end
@@ -109,23 +125,31 @@ plot(t,s_true,'r-','linewidth',2);
 end
 
 function Q = ExponentialCov(n,t,v,l)
+
 h = abs(repmat(t,1,n) - repmat(t',n,1));
 Q = v*exp(-h/l);
+
 end
 
 
 function Q=linearQ(n,x,var,l)
+
 theta = var/l;
 h=abs(repmat(x,1,n)-repmat(x',n,1));
 Q=-theta * h;
+
 end
 
 function Q=gaussianQ(n,x,var,L)
+
 h=abs(repmat(x,1,n)-repmat(x',n,1));
 Q=var*exp(-(h.^2/L^2));
+
 end
 
 function Q=smoothCov(n,t)
+
 h = abs(repmat(t,1,n) - repmat(t',n,1));
 Q = h.^3;
+
 end
